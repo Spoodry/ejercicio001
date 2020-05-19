05b4f7b833837563ff39a6dbe3985089b34d7f06
Spoodry <jan080599xd@gmail.com>
2020-05-18 23:45:47 -0500

    primer commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Ejercicio001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0) {
+                switch(int.Parse(args[0])) {
+                    case 0:
+                        System.Console.WriteLine("Conexión");
+                        String conexion = "datasource=127.0.0.1;port=3306;username=root;password=jpaf080599urf;database=mvc";
+
+                        String query = "SELECT * FROM alumnos";
+
+                        MySqlConnection databaseConnection = new MySqlConnection(conexion);
+                        MySqlCommand command = new MySqlCommand(query, databaseConnection);
+                        command.CommandTimeout = 60;
+                        
+                        MySqlDataReader reader;
+
+                        try {
+                            databaseConnection.Open();
+                            reader = command.ExecuteReader();
+
+                            if(reader.HasRows) {
+                                while(reader.Read()) {
+                                    System.Console.WriteLine(reader.GetString(0) + " " + reader.GetString(1) + " " + reader.GetString(2));
+                                }
+                            } else {
+                                Console.WriteLine("No se encontraron datos.");
+                            }
+
+                            databaseConnection.Close();
+
+                        } catch(Exception e) {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        break;
+                    default:
+                        System.Console.WriteLine("Opción no válida");
+                        break;
+                }
+
+            } else {
+                Console.WriteLine("Hello World! ");
+            }
+        }
+    }
+}
